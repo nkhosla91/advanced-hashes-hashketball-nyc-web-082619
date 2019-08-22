fbd675a2c01247a0dcb8b47fe05236ee5c9f7ec9
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-22 15:58:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,10 +115,10 @@ def num_points_scored(name)
   
   hash.each do |court, info|
     info.each do |facts, data|
-      #binding.pry
+      binding.pry
       if data.has_key?(name)
         return hash[info][facts][name][points]
-    end
+      end
     end
   end
 end
