ecf9a9d51d502e0dbc021d71bf39286cdc5d27b0
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-22 17:37:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ def player_stats(name)
     info[:players].each do |names|
       player_name = names[0].to_s.gsub("_", " ")
       binding.pry
-      if names[0].to_s == name
+      if player_name == name
         
       end
     end
