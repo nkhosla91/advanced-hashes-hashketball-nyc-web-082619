2866ee8612913a68e9e450b72394da7f1c3e8724
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-22 16:20:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,9 +140,9 @@ def team_colors(teamname)
   hash = game_hash
   hash.each do |court, info|
     if team_name = teamname
+      binding.pry
       return info[:colors]
     end
-    binding.pry
   end
 end
 
